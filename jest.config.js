4f28a71c4afedb608d57ff1578efae8c9a77e361
99-zziy <wldud8229@gmail.com>
2022-07-19 00:34:51 +0900

    :wrench: jest config 파일 수정

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
   testEnvironment: 'jsdom',
-  testMatch: ['**/*.test.(ts|tsx)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: [
+    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
