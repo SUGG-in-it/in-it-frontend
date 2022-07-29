b2c6b73f4bbf54fbdd9d2e0e8f3b1e03bfb6353a
99-zziy <wldud8229@gmail.com>
2022-07-29 20:27:24 +0900

    :wrench: eslint rules 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,23 +6,22 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  plugins: ["react", "@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
-  rules: {},
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['react', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
