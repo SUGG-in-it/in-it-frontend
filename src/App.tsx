05a395c0bb3b5cb67657fc91e8a5fb3d0d181d81
99-zziy <wldud8229@gmail.com>
2022-07-29 22:09:23 +0900

    :sparkles: 회원가입 화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-up" element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
