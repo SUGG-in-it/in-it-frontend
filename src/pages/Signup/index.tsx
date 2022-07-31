3d3e0790e2474f8aaa7805d33b6312a16f40f311
99-zziy <wldud8229@gmail.com>
2022-07-31 22:58:45 +0900

    :lipstick: 회원가입 버튼 스타일 수정

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -22,7 +22,9 @@ const SignUp = () => {
           <Input label="닉네임" type="text" placeholder="닉네임을 입력해주세요." />
           <Input label="비밀번호" type="password" placeholder="비밀번호를 입력해주세요." />
           <Input label="비밀번호 확인" type="password" placeholder="비밀번호 확인을 입력해주세요." />
-          <Button color={PointColor}>{'회원가입'}</Button>
+          <Button color={PointColor} margin={'3em 0em 1em'}>
+            {'회원가입'}
+          </Button>
           <LoginContainer>
             <span>이미 계정이 있습니까?</span>
             <u onClick={moveToLogin}>로그인하기</u>
