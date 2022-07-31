1e11baaeb50542cece4144b6fe339aaff72ab940
99-zziy <wldud8229@gmail.com>
2022-07-31 21:44:36 +0900

    :sparkles: 비밀번호 찾기파일, 404페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from './pages/Signup';
 import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
 import { GlobalStyle } from './styles/globalStyles';
+import NotFoundPage from '@/pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle></GlobalStyle>
       <Routes>
-        <Route path="/sign-up" element={<Signup />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
