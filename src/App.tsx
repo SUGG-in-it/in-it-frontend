6b8a3da2f3d4904d2d079063efdc520c5f6791c7
99-zziy <wldud8229@gmail.com>
2022-07-31 23:31:37 +0900

    :sparkles: recoil & error boundary & suspense 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './styles/globalStyles';
 import NotFoundPage from '@/pages/NotFound';
+import { RecoilRoot } from 'recoil';
+
+import Loading from './components/Loading';
+import Error from './components/Error';
 
 const SignUp = lazy(() => import('./pages/SignUp'));
 const SignIn = lazy(() => import('./pages/SignIn'));
 const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
 
-function App() {
+const App = () => {
+  return (
+    <RecoilRoot>
+      <GlobalStyle />
+      <ErrorBoundary FallbackComponent={Error}>
+        <Suspense fallback={<Loading />}>{routes()}</Suspense>
+      </ErrorBoundary>
+    </RecoilRoot>
+  );
+};
+
+const routes = () => {
   return (
     <BrowserRouter>
-      <GlobalStyle></GlobalStyle>
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
@@ -19,6 +34,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
