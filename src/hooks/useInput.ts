3f53a447ad774b09f27ad23134abf79a6c4aaadb
99-zziy <wldud8229@gmail.com>
2022-08-02 20:56:59 +0900

    :sparkles: input 관리하는 커스텀훅 생성

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export type InputType = {
+  value: string | number;
+  onChange: ({ target }: { target: HTMLInputElement }) => void;
+};
+
+const useInput = (initialValue: string | number): InputType => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = ({ target }: { target: HTMLInputElement }) => {
+    const { value } = target;
+    setValue(value);
+  };
+  return { value, onChange };
+};
+
+export default useInput;
