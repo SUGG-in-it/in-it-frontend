a07a45deee2f2302caf5083e64fdf823aba969b0
99-zziy <wldud8229@gmail.com>
2022-08-10 22:41:07 +0900

    :sparkles: editor 추가

--- src/pages/QuestionWrite/index.tsx
+++ src/pages/QuestionWrite/index.tsx
@@ -1,7 +1,28 @@
+import ToastEditor from '@/components/ToastEdtior';
 import styled from 'styled-components';
 
 const QuestionWritePage = () => {
-  return <div>{}</div>;
+  return (
+    <QuestionWriteContainer>
+      <ToastEditorWrapper>
+        <ToastEditor />
+      </ToastEditorWrapper>
+    </QuestionWriteContainer>
+  );
 };
+
+const QuestionWriteContainer = styled.div`
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 100px;
+`;
+
+const ToastEditorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 20%;
+  margin-right: 20%;
+`;
 
 export default QuestionWritePage;
