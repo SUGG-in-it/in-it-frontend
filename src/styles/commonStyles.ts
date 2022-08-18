39a4d0afd0cc4533d09ce9a6bf215cfa9aad41c2
99-zziy <wldud8229@gmail.com>
2022-08-18 16:46:56 +0900

    :lipstick: qlabel 스타일 commonstlyes로 이동

---
+++ src/styles/commonStyles.ts
@@ -0,0 +1,9 @@
+import { PrimaryColor } from '@/assets/colors';
+import styled from 'styled-components';
+
+export const QLable = styled.h1`
+  font-size: 3rem;
+  margin-right: 20px;
+  font-weight: bold;
+  color: ${PrimaryColor};
+`;
