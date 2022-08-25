a87aca7ddb2bbc537bd66c2d37e89771f3bdc8e4
99-zziy <wldud8229@gmail.com>
2022-08-25 13:28:22 +0900

    :adhesive_bandage: 500 error page 수정

--- src/ErrorrBoundary/CriticalErrorBoundary/index.tsx
+++ src/ErrorrBoundary/CriticalErrorBoundary/index.tsx
@@ -2,19 +2,22 @@ import { PropsWithChildren } from 'react';
 import { useQueryErrorResetBoundary } from 'react-query';
 import { ErrorBoundary } from 'react-error-boundary';
 import { CustomError } from '@/api/Error';
+import styled from 'styled-components';
+import ServerErrorImg from '@/assets/images/500error.png';
+import { PointColor } from '@/assets/colors';
 
 /* -------------------------------------------------------------------------------------------------
  * CriticalErrorBoundary ->  500 에러 등 중요한 에러를 처리하기 위한 ErrorBoundar
  * -----------------------------------------------------------------------------------------------*/
 
-function ErrorFallback({ error, resetErrorBoundary }) {
+const ErrorFallback = () => {
   return (
-    <div>
-      <h1> 데이터를 불러오는데 실패하였습니다. </h1>
-      <p> 에러가 지속되면 고객센터로 문의하세요. </p>
-    </div>
+    <ErrorContainer>
+      <img src={ServerErrorImg} />
+      <p>에러가 지속되면 wldud060960@gmail.com로 문의해주세요. </p>
+    </ErrorContainer>
   );
-}
+};
 
 const CriticalErrorBoundary = ({ children }: PropsWithChildren<unknown>) => {
   const { reset } = useQueryErrorResetBoundary();
@@ -26,7 +29,6 @@ const CriticalErrorBoundary = ({ children }: PropsWithChildren<unknown>) => {
         reset();
       }}
       onError={(error: CustomError) => {
-        console.log('CriticalErrorBoundary Error', error);
         if (error?.statusCode !== 500) {
           throw error;
         }
@@ -36,5 +38,22 @@ const CriticalErrorBoundary = ({ children }: PropsWithChildren<unknown>) => {
     </ErrorBoundary>
   );
 };
+
+const ErrorContainer = styled.div`
+  width: 100vw;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  img {
+    width: 80vw;
+  }
+  p {
+    font-weight: bold;
+    color: ${PointColor};
+  }
+`;
 
 export default CriticalErrorBoundary;
