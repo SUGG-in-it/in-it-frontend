1b1edc608daa22a91d7a751ba82cdba0a7833f6a
99-zziy <wldud8229@gmail.com>
2022-09-01 22:11:10 +0900

    :bug: 로그인한 유저는 로그인,회원가입  페이지 출입금지, 로그인안한 유저는 글쓰기, 마이페이지 출입금지

--- src/routes/AuthRoute.tsx
+++ src/routes/AuthRoute.tsx
@@ -1,17 +1,10 @@
 import { Navigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { loginState } from '@/store/users';
-import { verifyToken } from '@/api/auth';
 
 function Auth({ Component }) {
-  const setIsLogin = useSetRecoilState(loginState);
-
-  //TODO: auch 체크하는 부분 작성해야함
-  const authCheck = () => {
-    return true;
-  };
-
-  return authCheck() ? Component : <Navigate to="/" />;
+  const isLogin = useRecoilValue(loginState);
+  return isLogin ? Component : <Navigate to="/login" replace />;
 }
 
 export default Auth;
--- src/routes/UnAtuhRoute.tsx
+++ src/routes/UnAtuhRoute.tsx
@@ -4,7 +4,7 @@ import { loginState } from '@/store/users';
 
 function UnAuth({ Component }) {
   const isLogin = useRecoilValue(loginState);
-  return isLogin ? <Navigate to="/" /> : Component;
+  return isLogin ? <Navigate to="/" replace /> : Component;
 }
 
 export default UnAuth;
