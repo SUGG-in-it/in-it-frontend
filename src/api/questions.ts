3dad99ce27ce56789359c794afeff18079ef0c59
99-zziy <wldud8229@gmail.com>
2022-09-14 11:24:58 +0900

    :recycle: refactor question type

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -1,29 +1,7 @@
 import { GET } from '@/api/config/base';
+import { QusetionsRequestBody } from '@/types/request/questions';
 
-export interface GetQusetionsParams {
-  page: number;
-  count: number;
-  type: string;
-}
-
-interface QuestionResponseBody {
-  questions: Question[];
-}
-
-interface Question {
-  questionId: number;
-  title: string;
-  content: string;
-  nickname: string;
-  level: number;
-  point: number;
-  createdAt: string;
-  updatedAt: string;
-  isCompleted: boolean;
-  tagList: string[];
-}
-
-export const getQusetions = async (getQuestionsParams: GetQusetionsParams): Promise<QuestionResponseBody[]> => {
-  const { data } = await GET('/questions', getQuestionsParams);
-  return data.questions;
+export const getQusetions = async (qusetionsRequestBody: QusetionsRequestBody) => {
+  const { data } = await GET('/questions', qusetionsRequestBody);
+  return data;
 };
