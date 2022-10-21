9527176f1ec12e36462b574e3281958ca0c93fcb
99-zziy <wldud8229@gmail.com>
2022-10-21 19:41:57 +0900

    :bug: email 전송하기

---
+++ src/constants/validation.ts
@@ -0,0 +1,12 @@
+export const VALIDATION_ERROR_MSG = {
+  EMPTY_EMAIL: '이메일을 입력해주세요.',
+  EMPTY_PASSWORD: '비밀번호를 입력해주세요.',
+  EMPTY_CODE: '인증번호를 입력해주세요.',
+  EMPTY_NICKNAME: '닉네임을 입력해주세요.',
+  EMPTY_WORK_POSITION: '직무를 입력해주세요.',
+  EMPTY_TITLE: '제목을 입력해주세요.',
+  INVALID_EMAIL: '올바른 형식의 이메일을 입력해주세요.',
+  INVALID_PASSWORD: '최소 8자리 이상 영문, 숫자, 특수문자가 각각 1개 이상을 입력해주세요.',
+  INVALID_NICKNAME: '2자 이상 16자 이하, 영어 또는 숫자 또는 한글을 입력해주세요.',
+  INCONSISTENCY_PASSWORD: '비밀번호가 불일치 합니다',
+};
