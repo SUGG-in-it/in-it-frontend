ba0cfe9792fdc64f65dd9211867e5e79bf4a2c55
99-zziy <wldud8229@gmail.com>
2022-11-06 20:46:45 +0900

    :bug: fix image upload api

--- src/api/images.ts
+++ src/api/images.ts
@@ -3,5 +3,5 @@ import { POST } from '@/api/config/base';
 export const uploadImage = async (file: File) => {
   const uplaodFileForm = new FormData();
   uplaodFileForm.append('image', file);
-  return await POST(`/image/uploafdd/`, uplaodFileForm);
+  return await POST(`/image/upload/`, uplaodFileForm);
 };
