023495f740d3f0b1075a21044876528a89f5c810
99-zziy <wldud8229@gmail.com>
2022-11-09 17:08:55 +0900

    :sparkles: 모바일 일때 햄버거 메뉴 보이도록하기

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,12 +3,21 @@ import appLogo from '@/assets/images/bigLogo.png';
 import GrayLine from '@/components/common/GreyLine';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { media } from '@/styles/mediaQuery';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoMdClose } from 'react-icons/io';
+import { useState } from 'react';
 
 const Header = () => {
   const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogoClick = () => {
     router.push('/');
+  };
+
+  const handleClickMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
@@ -16,9 +25,29 @@ const Header = () => {
       <HeaderWrapper>
         <LeftSection>
           <LogoSection onClick={handleLogoClick}>
-            <Image src={appLogo} width={110} height={55} />
+            <Image src={appLogo} width={110} height={50} />
           </LogoSection>
         </LeftSection>
+        <RightSection>
+          <MenuIcon onClick={handleClickMenu} />
+          {isMenuOpen ? (
+            <Menu>
+              <MenuHeader>
+                <Image src={appLogo} width={110} height={50} />
+                <CloseButton onClick={handleClickMenu} />
+              </MenuHeader>
+              <ul>
+                <li>홈</li>
+                <li>답변하기</li>
+                <li>질문하기</li>
+                <li>마이페이지</li>
+                <li>
+                  <LogoutButton>로그아웃</LogoutButton>
+                </li>
+              </ul>
+            </Menu>
+          ) : null}
+        </RightSection>
       </HeaderWrapper>
       <GrayLine />
     </HeadeContainer>
@@ -37,10 +66,68 @@ const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  ${media.tablet} {
+    height: 60px;
+  }
 `;
 
 const LeftSection = styled.div`
   display: flex;
+`;
+
+const RightSection = styled.div``;
+
+const MenuHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1em 1.5em 2em;
+`;
+
+const CloseButton = styled(IoMdClose)`
+  width: 25px;
+  height: 25px;
+  color: ${({ theme }) => theme.primaryColor};
+`;
+
+const LogoutButton = styled.button`
+  border-radius: 3px;
+  background-color: ${({ theme }) => theme.primaryColor};
+  color: white;
+  padding: 0.5em;
+  width: 100%;
+  height: 40px;
+  cursor: pointer;
+  border: none;
+`;
+
+const MenuIcon = styled(GiHamburgerMenu)`
+  width: 25px;
+  height: 25px;
+  color: ${({ theme }) => theme.primaryColor};
+  cursor: pointer;
+  ${media.desktop} {
+    display: none;
+  }
+  ${media.tablet} {
+    display: block;
+  }
+`;
+
+const Menu = styled.div`
+  width: 100vw;
+  background-color: #f7f7f8;
+  position: fixed;
+  transition: all 500ms;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  box-sizing: border-box;
+  li {
+    font-weight: 600;
+    padding: 1.3em 1.8em;
+    border-bottom: 1px solid ${({ theme }) => theme.greyLineColor};
+  }
 `;
 
 const LogoSection = styled.div`
